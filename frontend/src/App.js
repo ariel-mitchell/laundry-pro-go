7f92d4123e8b803b5e554ac63323c0be0c8df5a9
Ariel Mitchell <ariel.l.makowski@gmail.com>
2023-12-17 01:33:09 -0500

    add order form with basic validation (non-react hook)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <ButtonAppBar/>
-      <OrderForm/>
+      <OrderForm />
       <Footer/>
 
     </div>
--- frontend/src/components/OrderForm.js
+++ frontend/src/components/OrderForm.js
@@ -1,17 +1,53 @@
 import * as React from 'react';
+import { useState } from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-// import CustomerAutoComplete from './CustomerAutoComplete';
+import Autocomplete, { createFilterOptions } from '@mui/material/Autocomplete';
+// import Alert from '@mui/material/Alert'
+// import Stack from '@mui/material/Stack'
+
 import styles from './OrderForm.module.css';
-import { useState } from 'react';
+
+const filter = createFilterOptions();
 
 export default function OrderForm() {
   const [orderNumber, setOrderNumber] = useState('');
 
-  const [customerName, setCustomerName] = useState('');
+  const [customer, setCustomer] = useState({
+    id: null,
+    name: ''
+  });
+
+  const [customers, setCustomers] = useState([]);
 
   const [orderDetails, setOrderDetails] = useState({
+    datePlaced: '',
+    bagsAtPickup: 0,
+    bagsAtDropoff: 0,
+    numberOfLoads: 0,
+    mileage: 0,
+    pounds: 0,
+    orderPayment: 0,
+    tip: 0,
+    notes: ''
+  });
+
+  const handleChange = (e) => {
+    if (e.target.name === "orderNumber") {
+      setOrderNumber(e.target.value);
+    } else {
+      setOrderDetails({
+        ...orderDetails,
+        [e.target.name]: e.target.value,
+      });
+    }
+  };
+
+  const reset = () => {
+    setOrderNumber('');
+    setCustomer({id: null, name: ''});
+    setOrderDetails({
       datePlaced: '',
       bagsAtPickup: 0,
       bagsAtDropoff: 0,
@@ -21,59 +57,131 @@ export default function OrderForm() {
       orderPayment: 0,
       tip: 0,
       notes: ''
-  });
-
-const orderObject = {orderNumber, customerName, orderDetails};
-
-const handleChange = (e) => {
-  
-  if (e.target.name === "orderNumber") {
-    setOrderNumber(e.target.value);
-  } else if (e.target.name === "customerName") {
-    setCustomerName(e.target.value);
-  } else {
-    setOrderDetails({
-      ...orderDetails,
-      [e.target.name]: e.target.value,
     });
-  }
-};
+  };
 
-function submitData(e) {
-  e.preventDefault();
-  fetch("http://localhost:8080/orders/add", {
-    method:"POST",
-    headers:{"Content-Type":"application/json"},
-    body:JSON.stringify(orderObject)
-  }).then(sus=>console.log(sus)).catch(e=>console.log(e));
-}
+  const orderObject = {orderNumber, customer, orderDetails};
+
+  let shouldAlert = false;
+
+  React.useEffect(() => {
+    fetch("http://localhost:8080/customers").then(res=>res.json()).then(result=>setCustomers(result)).catch(e=>console.log(e));
+  })
+
+  function submitData(e) {
+    e.preventDefault();
+    if (orderNumber === "" || (customer.id === 0 && customer.name === "") || (customer.id === null && customer.name === "") || orderDetails.datePlaced === "") {
+      window.alert("Please enter all required fields.")
+      shouldAlert=true;
+      return ;
+    }
+
+    fetch("http://localhost:8080/orders/add", {
+      method:"POST",
+      headers:{"Content-Type":"application/json"},
+      body:JSON.stringify(orderObject)
+    })
+    .then(sus=> {
+      console.log(sus);
+      shouldAlert=false;
+      reset();})
+    .catch(e=>console.log(e));
+
+  
+  };
 
   return (
     <div>
       <h1>Add An Order</h1>
-      <h4>Required Order Details</h4>
+      {/* TODO: add alert HTML elements instead of window alert? */}
+      {/* <Alert className={shouldAlert ? "alert" : "hidden"} variant="filled" severity="error">Order Number is Required!</Alert> */}
       <Box
       className={styles.container}
         component="form"
         sx={{
           '& > :not(style)': { m: 1, width: '25ch', padding:"10px" },
         }}
+        noValidate={false}
       >
-        <TextField required={true} name="orderNumber" label="Order Number" variant="outlined" onChange={handleChange} />
-        <TextField required={true} name="customerName" label="Customer Name" variant="outlined" onChange={handleChange} />
-        {/* <CustomerAutoComplete required={true} onChange={handleChange}/>
+        <TextField error={orderNumber ? false : true} name="orderNumber" label="Order Number" variant="outlined" id="outlined-error-helper-text" helperText="Order number required." value={orderNumber} onChange={handleChange} />
+        <Autocomplete
+          value={customer}
+          onChange={(event, newValue) => {
+            if (typeof newValue === 'string') {
+              setCustomer({
+                id: 0,
+                name: newValue
+              });
+            } else if (newValue && newValue.inputValue) {
+              // Create a new value from the user input
+              setCustomer({
+                id: 0,
+                name: newValue.inputValue
+              });
+              console.log(newValue);
+            } else {
+              setCustomer(newValue);
+            }
+          }}
+          filterOptions={(options, params) => {
+            const filtered = filter(options, params);
 
+            const { inputValue } = params;
+            // Suggest the creation of a new value
+            const isExisting = options.some((option) => inputValue === option.name);
+            if (inputValue !== '' && !isExisting) {
+              filtered.push({
+                inputValue,
+                name: `Add "${inputValue}"`,
+              });
+            }
+
+            return filtered;
+          }}
+          selectOnFocus
+          clearOnBlur
+          id="customer"
+          disableClearable={true}
+          handleHomeEndKeys
+          options={customers}
+          getOptionLabel={(option) => {
+            // Value selected with enter, right from the input
+            if (typeof option === 'string') {
+              return option;
+            }
+            // Add "xxx" option created dynamically
+            if (option.inputValue) {
+              return option.inputValue;
+            }
+            // Regular option
+            return option.name;
+          }}
+          renderOption={(props, option) => <li {...props}>{option.name}</li>}
+          sx={{ width: 300 }}
+          freeSolo
+          renderInput={(params) => (
+            <TextField {...params} label="Customer" error={customer.name ? false : true } id="outlined-error-helper-text" helperText="Customer required." />
+          )}
+        />
         {/* TODO: update field to be date input */}
-        <TextField required={true} label="Date Placed" name="datePlaced" variant="outlined" onChange={handleChange} />
-      <h4 style={{textAlign:"center"}}>Optional Order Details</h4>
-        <TextField label="Bags at Pickup" name="bagsAtPickup" variant="outlined" defaultValue="0" onChange={handleChange} />
-        <TextField label="Bags at Dropoff" name="bagsAtDropoff" variant="outlined" defaultValue="0" onChange={handleChange} />
-        <TextField label="Number of Loads" name="numberOfLoads" variant="outlined" defaultValue="0" onChange={handleChange} />
-        <TextField label="Total Mileage" name="mileage" variant="outlined" defaultValue="0.0" onChange={handleChange} /><br></br>
-        <TextField label="Total Pounds" name="pounds" variant="outlined" defaultValue="0.0"onChange={handleChange} />
-        <TextField label="Order Payment" name="orderPayment" variant="outlined" defaultValue="0.00"onChange={handleChange} />
-        <TextField label="Tip" name="tip" variant="outlined" defaultValue="0.00"onChange={handleChange} />
-        <TextField label="Notes" name="notes" variant="outlined" onChange={handleChange}/>
+        <TextField error={orderDetails.datePlaced ? false : true} label="Date Placed" name="datePlaced" variant="outlined" id="outlined-error-helper-text" helperText="Date required." value={orderDetails.datePlaced} onChange={handleChange} />
+      </Box>
+      <h4>Optional Order Details</h4>
+      <Box
+        sx={{
+          '& > :not(style)': { m: 1, width: '25ch', padding:"10px" },
+        }}
+        noValidate={true}
+      >
+        <TextField label="Bags at Pickup" name="bagsAtPickup" variant="outlined" value={orderDetails.bagsAtPickup} onChange={handleChange} />
+        <TextField label="Bags at Dropoff" name="bagsAtDropoff" variant="outlined" value={orderDetails.bagsAtDropoff} onChange={handleChange} />
+        <TextField label="Number of Loads" name="numberOfLoads" variant="outlined" value={orderDetails.numberOfLoads} onChange={handleChange} />
+        <TextField label="Total Mileage" name="mileage" variant="outlined" value={orderDetails.mileage} onChange={handleChange} /><br></br>
+        <TextField label="Total Pounds" name="pounds" variant="outlined" value={orderDetails.pounds}onChange={handleChange} />
+        {/* TODO: make these fields currency */}
+        <TextField label="Order Payment" name="orderPayment" variant="outlined" value={orderDetails.orderPayment} onChange={handleChange} />
+        <TextField label="Tip" name="tip" variant="outlined" value={orderDetails.tip} onChange={handleChange} />
+        <TextField label="Notes" name="notes" variant="outlined" value={orderDetails.notes} onChange={handleChange}/>
       </Box>
       <Box>
         <Button onClick={submitData} variant="contained">Sumbit</Button>
